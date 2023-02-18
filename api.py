83e46dee118dfe71c98407777ad16e899853d518
carboni-joseph <99344129+carboni-joseph@users.noreply.github.com>
2023-02-18 13:04:05 -0500

    fixed spelling error in mthd

--- api.py
+++ api.py
@@ -5,5 +5,5 @@ app = FastAPI()
 
 ### FRED-DATA ###
 @app.get("/fred-data")
-def get_fred_series_with_caluculated_data(series_id: str):
+def get_fred_series_with_calculated_data(series_id: str):
     return fred_data_enriched(series_id)
