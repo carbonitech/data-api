c13121291b3eb95886da66607a80a5f7eef02cdd
carboni-joseph <joe@carbonitech.com>
2023-06-22 13:21:39 -0400

    add auth0 auth on top of AI files endpoints

--- ai/app/main.py
+++ ai/app/main.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from ai.auth.auth import authenticate_auth0_token
 from ai.app.resources.files import files
 from ai.app.resources.chat import chat
 
 app = APIRouter(prefix='/ai', tags=['AI Chatbot'])
-app.include_router(files)
+app.include_router(files, dependencies=[Depends(authenticate_auth0_token)])
 app.include_router(chat)
 
