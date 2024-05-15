0d60d288886ab1e27f90e722b351d3953d9dd8f0
carboni-joseph <joe@carbonitech.com>
2024-05-15 12:14:32 -0400

    disable ai send_query endpoint

--- ai/app/resources/chat.py
+++ ai/app/resources/chat.py
@@ -14,6 +14,9 @@ class AIResponse(BaseModel):
 
 @chat.post('')
 async def send_query(query: Query, ai: AI=Depends(get_ai)) -> AIResponse:
+    # NOTE: since OpenAI switched to pre-payment, and I haven't set it
+    #       up yet, this service is not going to work
+    raise HTTPException(status_code=503)
     query_txt = query.query
     gpt_response = ai.ask_model(query=query_txt)
     return AIResponse(query=query.query, response=gpt_response)
