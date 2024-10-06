70eb79b54834d288b76f68e71a2fedfd10191057
carboni-joseph <joe@carbonitech.com>
2024-10-06 07:41:00 -0400

    setting up an endpoint to play around with power automate requests coming to a FastAPI app

---
+++ testing/mspa.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter, Request, UploadFile
+
+mspa = APIRouter("/mspa")
+
+
+@mspa.get("")
+def get_req(r: Request):
+    for header, value in r.headers.items():
+        print(f"\t{header}: {value}")
+
+
+@mspa.post("")
+def post_req(r: Request, file: UploadFile = None):
+    print("--------- HEADERS ---------")
+    for header, value in r.headers.items():
+        print(f"\t{header}: {value}")
+    print("--------- BODY ---------")
+    print(r.body())
